3a847d6035c5fa8c903b616187ef80db5a9269fc
Timeless <timelessrecall@gmail.com>
2025-07-01 15:12:59 +0530

    Update fibonacci_series.c

--- programs/fibonacci_series.c
+++ programs/fibonacci_series.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+
 int main() {
+    int i, n;
+    int term1 = 0, term2 = 1;
+    int next_term = term1 + term2;
 
-  int i, n;
-  int t1 = 0, t2 = 1;
-  int sum = t1 + t2;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
 
-  printf("Enter the number of terms: ");
-  scanf("%d", &n);
+    printf("Fibonacci Series: ");
 
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
+    if (n == 1) {
+        printf("%d", term1);
+    } else if (n >= 2) {
+        printf("%d, %d", term1, term2);
 
-  for (i = 3; i <= n; ++i) {
-    printf("%d, ", sum);
-    t1 = t2;
-    t2 = sum;
-    sum = t1 + t2;
-  }
+        for (i = 3; i <= n; ++i) {
+            printf(", %d", next_term);
+            term1 = term2;
+            term2 = next_term;
+            next_term = term1 + term2;
+        }
+    }
 
-  return 0;
+    printf("\n");
+
+    return 0;
 }
